bf080f7818edb8618526ed5e16dbe8f54238e31e
Malia Porter <maliaporter2024@gmail.com>
2024-04-24 12:16:02 -0400

    started creating grid for about us page

---
+++ templates/head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Bookish</title>
+  
+    <!-- Meta -->
+    <meta name="keywords" content="books, reading, readers">
+    <meta name="description" content="Web Application for Readers to Track Their Reading Process">
+    <meta name="author" content="Caleigh Wlazlowski, Malia Porter">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+  
+    <!-- CSS -->
+    <link rel='stylesheet' type='text/css' href='/styles/index.css'>
+  
+    <!-- Icon -->
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+  
+  </head>
+</html>
